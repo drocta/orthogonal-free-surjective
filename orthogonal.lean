74d80226e611f135213b7348d2237bf94fd400c5
drocta <madaco.madaco@gmail.com>
2019-03-03 17:37:19 -0800

    upgrading to use the version of mathlib for lean 3.4.2 instead of 3.4.1, which requires some changes to how category is used.

--- orthogonal.lean
+++ orthogonal.lean
@@ -7,14 +7,14 @@ open category_theory
 universes u_1 v_1
 
 section
-variables (C : Type u_1) [CC : category.{u_1 v_1} C]
+variables (C : Type u_1) [CC : category.{v_1 u_1} C]
 include CC
 
-def orthogonal {A B X Y : C} (i : category.hom A B) (p : category.hom X Y) :=
-	forall (f : category.hom A X) (g : category.hom B Y) ,
-		(category.comp f p = category.comp i g) ->
-		exists (h : category.hom B X) ,
-			(and (category.comp i h = f) (category.comp h p = g))
+def orthogonal {A B X Y : C} (i : has_hom.hom A B) (p : has_hom.hom X Y) :=
+	forall (f : has_hom.hom A X) (g : has_hom.hom B Y) ,
+		(category_struct.comp f p = category_struct.comp i g) ->
+		exists (h : has_hom.hom B X) ,
+			(and (category_struct.comp i h = f) (category_struct.comp h p = g))
 end
 
 #check orthogonal
